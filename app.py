e2177e1e3ae0bd43e46dcabc87bea26f2f33a3ea
Joshua Gomes <gomesjoshua@gmail.com>
2021-09-20 20:26:24 -0700

    Input validation complete. Html files split into index and result. Everything working well.

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from os.path import join, dirname
 from dotenv import load_dotenv, find_dotenv
 import requests
 from isodate import parse_duration
+from flask_wtf import FlaskForm
+from forms import Queryform, csrf
+from wtforms import StringField, IntegerField, ValidationError, SubmitField
+from wtforms.validators import InputRequired, NumberRange
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thecodex'
@@ -14,78 +19,77 @@ path = '/Users/joshuagomes/YouTube/.env/key.py'
 load_dotenv(dotenv_path=path,verbose=True)
 
 class Config:
-    print("Inside the Config file")
     API_KEY = os.environ.get("KEY")
 
 c = Config()
 key = c.API_KEY
-print(key)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+    form = Queryform()
+    if request.method == 'POST':
+        form = Queryform()
+        if form.validate_on_submit():
+            return redirect(url_for("result", query=form.searchTerm.data, maxResults=form.maxResults.data))
+
+    return render_template("base.html", form=form)
+
+@app.route("/results/<string:query>/<int:maxResults>", methods=['GET', 'POST'])
+#@app.route("/results", methods=['GET', 'POST'])
+def result(query, maxResults):
     search_url = 'https://www.googleapis.com/youtube/v3/search'
     video_url = 'https://www.googleapis.com/youtube/v3/videos'
 
     videos = []
-    max_num = request.form.get('maxResults')
-    query = request.form.get('searchTerm')
-    if max_num == '':
-        max_num = 1
-    if request.form.get('searchTerm') == '':
-        query = "Hello World"
+    max_num = maxResults
+    query = query
 
+    search_params = {
+        'key' : key,
+        'q' : query,
+        'part' : 'snippet',
+        'maxResults' : max_num,
+        'relevanceLanguage' : 'en',
+        'type' : 'video'
+    }
 
-    if request.method == 'POST':
-        search_params = {
-            'key' : key,
-            'q' : query,
-            'part' : 'snippet',
-            'maxResults' : max_num,
-            'relevanceLanguage' : 'en',
-            'type' : 'video'
+    r = requests.get(search_url, params=search_params)
+
+    results = r.json()['items']
+
+    video_ids = []
+    for result in results:
+        video_ids.append(result['id']['videoId'])
+
+    video_params = {
+        'key' : key,
+        'id' : ','.join(video_ids),
+        'part' : 'snippet,contentDetails, statistics',
+        'maxResults' : max_num
+    }
+
+    r = requests.get(video_url, params=video_params)
+    results = r.json()['items']
+    str1 = ","
+    for result in results:
+        video_data = {
+            'id' : result['id'],
+            'url' : f'https://www.youtube.com/watch?v={ result["id"] }',
+            'thumbnail' : result['snippet']['thumbnails']['high']['url'],
+            'duration' : int(parse_duration(result['contentDetails']['duration']).total_seconds() // 60),
+            'title' : result['snippet']['title'],
+            'views' : result['statistics']['viewCount'],
+            'likes' : result['statistics']['likeCount'] if 'likeCount' in result['statistics'] else 'NONE',
+            'dislikes' : result['statistics']['dislikeCount'] if 'dislikeCount' in result['statistics'] else 'NONE',
+            'tags' : str1.join(result['snippet']['tags']) if 'tags' in result['snippet'] else 'NONE',
         }
-        print(search_params)
-
-        r = requests.get(search_url, params=search_params)
-
-        results = r.json()['items']
-
-        video_ids = []
-        for result in results:
-            video_ids.append(result['id']['videoId'])
-
-        video_params = {
-            'key' : key,
-            'id' : ','.join(video_ids),
-            'part' : 'snippet,contentDetails, statistics',
-            'maxResults' : max_num
-        }
-
-        r = requests.get(video_url, params=video_params)
-        results = r.json()['items']
-        str1 = ","
-        for result in results:
-            print("VIDEO RESULTS:")
-            video_data = {
-                'id' : result['id'],
-                'url' : f'https://www.youtube.com/watch?v={ result["id"] }',
-                'thumbnail' : result['snippet']['thumbnails']['high']['url'],
-                'duration' : int(parse_duration(result['contentDetails']['duration']).total_seconds() // 60),
-                'title' : result['snippet']['title'],
-                'views' : result['statistics']['viewCount'],
-                'likes' : result['statistics']['likeCount'] if 'likeCount' in result['statistics'] else 'NONE',
-                'dislikes' : result['statistics']['dislikeCount'] if 'dislikeCount' in result['statistics'] else 'NONE',
-                'tags' : str1.join(result['snippet']['tags']) if 'tags' in result['snippet'] else 'NONE',
-            }
-            videos.append(video_data)
+        videos.append(video_data)
 
 
-        print("DF")
-        df = pd.DataFrame(videos)
-        print(df)
-        df.to_csv('./static/search_results.csv', sep='\t', encoding='utf-8', index=False)
+    df = pd.DataFrame(videos)
+    df.to_csv('./static/search_results.csv', sep='\t', encoding='utf-8', index=False)
 
-    return render_template('base.html', videos=videos)
+    return render_template('results.html', videos=videos)
 
 @app.route("/downloadCSV", methods=['GET'])
 def download():
@@ -98,4 +102,4 @@ def download():
                  "attachment; filename=search_results.csv"})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,9 +1,13 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, CsrfProtect
 from wtforms import StringField, PasswordField, SubmitField, IntegerField, validators, ValidationError
+from wtforms.validators import InputRequired, NumberRange
 
+csrf = CsrfProtect()
 
-class SignUpForm(FlaskForm):
-    searchTerm = StringField(label='', validators=[validators.DataRequired()])
-    maxResults = StringField(label='', validators=[validators.DataRequired()])
-    submit = SubmitField('Submit')
-
+class Queryform(FlaskForm):
+    print("Inside query form")
+    searchTerm = StringField(label='searchTerm', validators=[InputRequired('A query is required')])
+    maxResults = StringField(label='maxResults', validators=[InputRequired('Please enter a valid number')])
+    #maxResults = IntegerField('maxResults', validators=[InputRequired('Please enter a valid number'), NumberRange(min=0, max=100, message='Please enter a number from 0 to 100')])
+    #submit = SubmitField('Submit')
+    print(f'SearchTerm:{searchTerm}, max_results: {maxResults}')
