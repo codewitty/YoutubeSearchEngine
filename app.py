5c02f764f1352844bc83f498457427db442ae1c1
Joshua Gomes <gomesjoshua@gmail.com>
2021-09-20 20:58:38 -0700

    download fields adjusted per user request

--- app.py
+++ app.py
@@ -71,23 +71,36 @@ def result(query, maxResults):
     r = requests.get(video_url, params=video_params)
     results = r.json()['items']
     str1 = ","
+    excel_list = []
     for result in results:
         video_data = {
             'id' : result['id'],
             'url' : f'https://www.youtube.com/watch?v={ result["id"] }',
             'thumbnail' : result['snippet']['thumbnails']['high']['url'],
             'duration' : int(parse_duration(result['contentDetails']['duration']).total_seconds() // 60),
-            'title' : result['snippet']['title'],
-            'views' : result['statistics']['viewCount'],
-            'likes' : result['statistics']['likeCount'] if 'likeCount' in result['statistics'] else 'NONE',
-            'dislikes' : result['statistics']['dislikeCount'] if 'dislikeCount' in result['statistics'] else 'NONE',
-            'tags' : str1.join(result['snippet']['tags']) if 'tags' in result['snippet'] else 'NONE',
+            'title' : result['snippet']['title'] if 'title' in result['snippet'] else 'NONE',
         }
         videos.append(video_data)
 
+    for result in results:
+        video_data = {
+            'Title' : result['snippet']['title'] if 'title' in result['snippet'] else 'NONE',
+            'Link' : f'https://www.youtube.com/watch?v={ result["id"] }',
+            'Views' : result['statistics']['viewCount'] if 'viewCount' in result['statistics'] else 'NONE',
+            'Year' : result['snippet']['publishedAt'] if 'publishedAt' in result['snippet'] else 'NONE',
+            'Tags' : str1.join(result['snippet']['tags']) if 'tags' in result['snippet'] else 'NONE',
+            'Description' : result['snippet']['description'] if 'description' in result['snippet'] else 'NONE',
+            'Uploader Name' : result['snippet']['channelTitle'] if 'channelTitle' in result['snippet'] else 'NONE',
+            'Likes' : result['statistics']['likeCount'] if 'likeCount' in result['statistics'] else 'NONE',
+            'Dislikes' : result['statistics']['dislikeCount'] if 'dislikeCount' in result['statistics'] else 'NONE',
+        }
+        excel_list.append(video_data)
 
-    df = pd.DataFrame(videos)
+
+    df = pd.DataFrame(excel_list)
+    print(df)
     df.to_csv('./static/search_results.csv', sep='\t', encoding='utf-8', index=False)
+    #df.to_excel('./static/search_results.xlsx', encoding='utf-8', index=False)
 
     return render_template('results.html', videos=videos)
 
